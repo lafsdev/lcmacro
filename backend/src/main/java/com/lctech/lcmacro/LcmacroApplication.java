393576bd9e39618b098bd8ce6527de5efc21a02f
Leandro Alves de Fontes Silva <lafs.dev@gmail.com>
2022-01-30 17:32:41 -0300

    Project created

---
+++ backend/src/main/java/com/lctech/lcmacro/LcmacroApplication.java
@@ -0,0 +1,13 @@
+package com.lctech.lcmacro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcmacroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcmacroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lctech/lcmacro/LcmacroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lctech.lcmacro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcmacroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
